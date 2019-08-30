a56ebb8402e0403fd5fd29e3af724ba68671725e
HirudineeADRT <hirudinee@adroitlogic.com>
2019-08-30 14:36:54 +0530

    firsts

---
+++ index.js
@@ -0,0 +1,4 @@
+// This file is used to register all your cloud functions in GCP.
+// DO NOT EDIT/DELETE THIS, UNLESS YOU KNOW WHAT YOU ARE DOING.
+
+exports.hiru3008vcsmergefunction = require("./hiru3008vcsmerge/function.js").handler;
